dba13866e675eeebdd9c352f798e91c57bbd01a8
Daniel Salomon <dsalomo8@pratt.edu>
2021-04-21 15:32:53 -0400

    master

---
+++ prey.js
@@ -0,0 +1,82 @@
+const video = document.querySelector('#camera-live');
+const hiddenCanvas = document.querySelector('#hidden-canvas');
+const outputCanvas = document.querySelector('#output-canvas');
+const hiddenContext = hiddenCanvas.getContext('2d');
+const outputContext = outputCanvas.getContext('2d');
+const getImageData = outputCanvas.getContext('2d');
+
+const constraints = {
+  video: {
+    width: 1920,
+    height: 1080,
+  },
+};
+
+const getAverageRGB = (frame) => {
+  const length = frame.data.length / 12; //Frame rate
+
+  let r = 0;
+  let g = 0;
+  let b = 0;
+
+  for (let i = 0; i < length; i++) {
+    r += frame.data[i * 1 + 0]; //1-black 2-green 4-brown 8-color
+    g += frame.data[i * 1 + 1]; 
+    b += frame.data[i * 1 + 2];
+  }
+
+  return {
+    r: r / length,
+    g: g / length,
+    b: b / length,
+  };
+};
+
+const charset = '01'; // Characters/digits
+
+const processFrame = () => {
+  const fontHeight = 18; // Font size
+  const { videoWidth: width, videoHeight: height } = video;
+
+  if (width && height) {
+    hiddenCanvas.width = width; //Digit canvas
+    hiddenCanvas.height = height;
+    outputCanvas.width = width; //Digit canvas area
+    outputCanvas.height = height;
+    hiddenContext.drawImage(video, 0, 0, width, height);
+
+    outputContext.textBaseline = 'center';
+    outputContext.font = `${fontHeight}px Arial`;
+
+    const text = outputContext.measureText('0');
+    const fontWidth = parseInt(text.width);
+
+    outputContext.clearRect(0, 0, width, height);
+
+    for (let y = 0; y < height; y += fontHeight) {
+      for (let x = 0; x < width; x += fontWidth) {
+        const frameSection = hiddenContext.getImageData(x, y, fontWidth, fontHeight);
+        const { r, g, b } = getAverageRGB(frameSection);
+        const randomCharacter = charset[Math.floor(Math.random() * charset.length)];
+
+        outputContext.fillStyle = `rgb(${r},${g},${b})`;
+        outputContext.fillText(randomCharacter, x, y); //Numbers Output
+        outputContext.fillRect(x, y, fontWidth, fontHeight); // Pixelated Output
+      }
+    }
+  }
+
+  window.requestAnimationFrame(processFrame);
+};
+
+navigator.getUserMedia(constraints, function (stream) {
+  video.srcObject = stream;
+  video.play();
+}, function (err) {
+  console.error(err);
+});
+
+video.addEventListener('play', function () {
+  window.requestAnimationFrame(processFrame);
+  console.log('Live!');
+});
